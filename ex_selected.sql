707f31421bbeab2080df6fd1d799639aecf6a31f
illupovero <davideluporini1996@gmail.com>
2022-03-09 15:55:56 +0100

    ex 2-3-4-5

--- ex_selected.sql
+++ ex_selected.sql
@@ -2,3 +2,30 @@
 SELECT * FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990  
 ORDER BY `students`.`date_of_birth`  DESC
+
+
+-- Selezionare tutti i corsi che valgono più di 10 crediti
+
+SELECT * FROM `courses`
+WHERE `cfu` >= 10  
+ORDER BY `courses`.`cfu`  DESC
+
+-- Selezionare tutti gli studenti che hanno più di 30 anni
+
+SELECT * FROM `students` 
+
+WHERE YEAR(`date_of_birth`) =  1992 
+ORDER BY `students`.`date_of_birth`  DESC
+
+-- Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea
+
+SELECT * FROM `courses` 
+WHERE `period` = "I semestre"  
+ORDER BY `courses`.`period`  DESC
+
+-- Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+SELECT * FROM `exams`
+
+WHERE `hour` >= "14:00"   AND `date`  = "2020/06/20"
+ORDER BY `exams`.`date` DESC
+
